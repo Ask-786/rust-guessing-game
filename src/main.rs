1593431d8a2c22da569a7e871a2f9034a2344bd8
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2024-08-07 09:02:52 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::{io, u32};
+
+fn main() {
+    let randome_number = rand::thread_rng().gen_range(1..101);
+    let mut message = "Please enter a number".normal();
+    let mut score: u32 = 0;
+
+    println!(
+        "{}",
+        format!(
+            "You have to guess a number, and we will guide you if the
+number you entered is higher or lesser.
+and you will get a result based on your number of guesses,
+{}",
+            "The lesser, the better. Let's start!!".yellow()
+        )
+    );
+
+    loop {
+        let mut guess = String::new();
+
+        println!("\n{}", message);
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed reading the input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                message = "Invalid input, try again".normal();
+                continue;
+            }
+        };
+
+        score += 1;
+
+        match guess.cmp(&randome_number) {
+            Ordering::Less => message = "That's too low, try again".red(),
+            Ordering::Greater => message = "That's a bit greater, try again".red(),
+            Ordering::Equal => {
+                let score = score.to_string();
+                println!(
+                    "{}",
+                    format!("\nCongratulations You got it!!, Your Score: {}", score).green()
+                );
+                break;
+            }
+        }
+    }
+}
